c6ac0010c5bafa765ff9a00d7c7c4b2e05f71341
TemurMalik <tmaq77@gmail.com>
2026-03-14 14:18:20 +0500

    Edit fayl

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import jsPDF from "jspdf";
+import logo from "../../vite-project/src/assets/hero.png"
 
 function App() {
   function PDFdowload() {
     let doc = new jsPDF();
+
+    doc.addImage(logo, "png", 10, 10, 50, 50);
 
     doc.setTextColor(123, 33, 44);
     doc.setFontSize(100);
